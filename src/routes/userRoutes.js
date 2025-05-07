c47c95df83bcf056b5d26314b840cb6b4dd939cd
isaacudofia <isaacudofia0@gmail.com>
2025-05-07 19:55:06 +0100

    Validate User using Joi package

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,11 +7,12 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/userControllers.js";
+import validateUser from "../middlewares/inputValidator.js";
 
 router.get("/user", getAllUsers);
 router.get("/user/:id", getUserById);
-router.post("/user", createUser);
-router.put("/user/:id", updateUser);
+router.post("/user", validateUser, createUser);
+router.put("/user/:id", validateUser, updateUser);
 router.delete("/user/:id", deleteUser);
 
 export default router;
